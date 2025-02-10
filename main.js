a5e84d29147937b8a70391393264eeabdfd7748f
erionazemi <167331736+erionazemi@users.noreply.github.com>
2025-02-10 15:27:05 +0100

    drj

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
+function changeSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  const slideWidth = slides[0].clientWidth;
+  const slider = document.querySelector('.slider');
+  slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+setInterval(changeSlide, 3000);
